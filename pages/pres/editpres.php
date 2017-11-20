bc8022fbb9a45c2ccee872ca7954232f69155902
Bryan Javier Morales <jgodoy94@gmail.com>
2017-11-20 02:14:01 -0600

    prestamos al 80%

--- pages/pres/editpres.php
+++ pages/pres/editpres.php
@@ -8,8 +8,8 @@
     <head>
         <title>CoopSys</title>
         <meta charset="utf8">        
-        <link rel="stylesheet" type="text/css" href="../styles/general.css">
-        <link rel="stylesheet" type="text/css" href="../styles/pres.css">
+        <link rel="stylesheet" type="text/css" href="../../styles/general.css">
+        <link rel="stylesheet" type="text/css" href="../../styles/pres.css">
     </head>
     <body>
         <div id="principal-container">
@@ -19,18 +19,58 @@
             </header>
             <nav>
                 <ul>
-                    <li><a href="afi.php">Afiliados</a></li>
-                    <li><a href="pres.php">Préstamos</a></li>
-                    <li><a href="cue.php">Cuentas</a></li>
-                    <li><a href="gen.php">Gerencia</a></li>
+                    <li><a href="../afi.php">Afiliados</a></li>
+                    <li><a href="../pres.php">Préstamos</a></li>
+                    <li><a href="../cue.php">Cuentas</a></li>
+                    <li><a href="../gen.php">Gerencia</a></li>
                 </ul>
             </nav>
             <div id="principal-body">
                 <div class="busqueda">
-                    <form action="pres.php" method="post">
-                        <input type="text" required placeholder="Codigo de Afiliado" name="codeafi">
-                        <input type="submit" value="BUSCAR">
-                    </form>
+                    <?php
+                        if(isset($_SESSION['tempsoli']) && !empty($_SESSION['tempsoli'])){
+                            require("../../models/nnoc.php");
+                            $soli = $_SESSION['tempsoli'];
+                            $query = "select * from lbtserp where liso = $soli and serpest = 'f'";
+                            $tluser = pg_query($nnoc, $query);
+                            if($tluser){
+                                if(pg_num_rows($tluser) > 0){
+                                    while( $obj = pg_fetch_object($tluser) ){
+                                        $tomon = $obj->tomon;
+                                        $satouc = $obj->satouc;
+                                        $roirp = $obj->roirp;
+                                    }
+
+                                    echo "
+                                    <form action='editpres.php' method='post' class='formedit'>
+                                        <h3>EDICION DE SOLICITUD $soli</h3>
+                                        <label>MONTO: </label><input type='text' required placeholder='$tomon' name='tomon'>
+                                        <label>CUOTAS: </label><input type='text' required placeholder='$satouc' name='satouc'>
+                                        <label>PRIORIDAD: </label><input type='text' required placeholder='$roirp' name='roirp'>
+                                        <input type='submit' value='GUARDAR CAMBIOS'>
+                                    </form>
+                                    ";
+                                }
+                                else{
+                                    echo "
+                                    <h3>NO SE PUEDEN EDITAR LOS DATOS DE ESTA SOLICITUD DE PRESTAMOS</h3>
+                                    <h4>LA SOLICITUD $soli NO SE ENCUENTRA DISPONIBLE PARA EDICION YA QUE SE ENCUNETRA ACTIVA</h4>
+                                    ";
+                                }
+                            }
+                            else{
+                                echo "
+                                <h3>NO SE PUEDEN EDITAR LOS DATOS DE ESTA SOLICITUD DE PRESTAMOS</h3>
+                                <h4>ACCESO DENEGADO PARA EDICION DE LA SOLICITUD $soli</h4>
+                                ";
+                            }
+                        }
+                        else{
+                            echo "
+                            <h3>NO SE RECIBIO NINGUNA SOLICITUD PARA EDICION</h3>
+                            ";
+                        }
+                    ?>
                 </div>
             </div>
         </div>
